3c7285e4a5260f5ae99ee2731e3716cc8531c79f
Yereth <yereth.espinoza.solis@est.una.ac.cr>
2025-12-28 01:43:15 -0600

    Fix: Remove react-native-vector-icons config and update .gitignore
    - Remove react-native-vector-icons manual config (not needed in RN 0.73+)
    - Add Xcode.app and iOS build artifacts to .gitignore
    - Add temporary scripts to .gitignore

--- mobile/react-native.config.js
+++ mobile/react-native.config.js
@@ -4,14 +4,4 @@ module.exports = {
     android: {},
   },
   assets: ['./src/assets/fonts/'],
-  dependencies: {
-    'react-native-vector-icons': {
-      platforms: {
-        ios: {
-          sourceDir: '../node_modules/react-native-vector-icons/ios',
-          project: 'RNVectorIcons.xcodeproj',
-        },
-      },
-    },
-  },
 };
